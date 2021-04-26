55b18e03af8a41aa5f04af5181b00519c4427079
dazeyb <btdazey@gmail.com>
2021-04-25 18:51:40 -0700

    Changed year to title for pushing up index

--- js/app.js
+++ js/app.js
@@ -215,7 +215,7 @@ const printOdds = function printOdds (arr) {
     for(let i = 0; i < arr.length; i++ ) {
     
     if(arr[i].year % 2 != 0) {
-    oddBonds.push(arr[i].year);
+    oddBonds.push(arr[i].title);
   }
   }
   return oddBonds;
